3391327b1bacbf51e601de51ae3395b9ec5c599b
AnnasyaMZ <nasyyzhra@gmail.com>
2026-06-16 05:06:52 +0700

    tambah provoder

--- Praktikum/laprak14/lib/main.dart
+++ Praktikum/laprak14/lib/main.dart
@@ -1,179 +1,69 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'character_model.dart';
-import 'db_helper.dart';
+import 'package:provider/provider.dart';
+import 'counter_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SQLite & HTTP Demo',
-      theme: ThemeData.dark(useMaterial3: true),
-      home: const CharacterListScreen(),
+      title: 'Flutter Counter with Provider',
+      home: const CounterScreen(),
     );
   }
 }
 
-class CharacterListScreen extends StatefulWidget {
-  const CharacterListScreen({super.key});
-
-  @override
-  State<CharacterListScreen> createState() => _CharacterListScreenState();
-}
-
-class _CharacterListScreenState extends State<CharacterListScreen> {
-  List<Character> _characters = [];
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadFromLocal();
-  }
-
-  Future<void> _loadFromLocal() async {
-    setState(() => _isLoading = true);
-    final localData = await DatabaseHelper.instance.getAllCharacters();
-    setState(() {
-      _characters = localData;
-      _isLoading = false;
-    });
-  }
-
-  // Fungsi Menghapus Semua Data dari SQLite
-  Future<void> _deleteAllData() async {
-    bool? confirm = await showDialog<bool>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Hapus Semua Data?'),
-        content: const Text('apakah anda yakin?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context, false),
-            child: const Text('Batal'),
-          ),
-          TextButton(
-            onPressed: () => Navigator.pop(context, true),
-            child: const Text('Hapus', style: TextStyle(color: Colors.red)),
-          ),
-        ],
-      ),
-    );
-
-    if (confirm != true) return;
-
-    setState(() => _isLoading = true);
-
-    try {
-      await DatabaseHelper.instance.clearTable();
-
-      setState(() {
-        _characters.clear();
-      });
-
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Semua data berhasil dihapus dari sistem.'),
-          ),
-        );
-      }
-    } catch (e) {
-      if (mounted) {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text('Terjadi kesalahan: $e')));
-      }
-    } finally {
-      setState(() => _isLoading = false);
-    }
-  }
-
-  Future<void> _fetchFromAPI() async {
-    setState(() => _isLoading = true);
-
-    try {
-      final response = await http.get(
-        Uri.parse('https://jsonplaceholder.typicode.com/users'),
-      );
-
-      if (response.statusCode == 200) {
-        final List<dynamic> dataJson = json.decode(response.body);
-
-        await DatabaseHelper.instance.clearTable();
-
-        List<Character> freshCharacters = [];
-
-        for (var item in dataJson) {
-          final char = Character.fromMap(item);
-          freshCharacters.add(char);
-          await DatabaseHelper.instance.insertCharacter(char);
-        }
-
-        setState(() => _characters = freshCharacters);
-
-        if (mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Berhasil sync data dari server!')),
-          );
-        }
-      }
-    } catch (e) {
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Gagal konek internet, pastikan online.'),
-          ),
-        );
-      }
-    } finally {
-      setState(() => _isLoading = false);
-    }
-  }
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ensiklopedia NPC'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.delete_forever, color: Colors.redAccent),
-            tooltip: 'Hapus Semua Data',
-            onPressed: _characters.isEmpty ? null : _deleteAllData,
+        title: const Text("Ini adalah aplikasi Counter"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Isi Counter: "),
+            Consumer<CounterProvider>(
+              builder: (context, counter, child) {
+                return Text("${counter.counter}");
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Provider.of<CounterProvider>(context, listen: false).increment();
+            },
+            child: const Icon(Icons.add),
           ),
-          IconButton(
-            icon: const Icon(Icons.cloud_download),
-            tooltip: 'Sync dari Server',
-            onPressed: _fetchFromAPI,
+          const SizedBox(
+            height: 10,
+          ), // Tambahan sedikit jarak biar tombol tidak menempel
+          FloatingActionButton(
+            onPressed: () {
+              Provider.of<CounterProvider>(context, listen: false).decrement();
+            },
+            child: const Icon(Icons.remove),
           ),
         ],
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : _characters.isEmpty
-          ? const Center(child: Text('Data kosong.'))
-          : ListView.builder(
-              itemCount: _characters.length,
-              itemBuilder: (context, index) {
-                final char = _characters[index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.deepPurple,
-                    child: Text(char.id.toString()),
-                  ),
-                  title: Text(char.name),
-                  subtitle: Text('Username: ${char.username}'),
-                );
-              },
-            ),
     );
   }
 }
